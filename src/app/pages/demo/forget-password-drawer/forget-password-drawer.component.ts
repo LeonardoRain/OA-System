867305afd62901948e369a26ef99d606bc43a910
LeonardoRain <295497522@qq.com>
2020-04-07 00:51:57 +0800

    修改bug

--- src/app/pages/demo/forget-password-drawer/forget-password-drawer.component.ts
+++ src/app/pages/demo/forget-password-drawer/forget-password-drawer.component.ts
@@ -1,23 +1,18 @@
-import {
-  Component
-}
+import { Component } from '@angular/core';
 
-from '@angular/core';
-
-@Component( {
-    selector: 'app-forget-password-drawer',
-    templateUrl: './forget-password-drawer.component.html',
-    styleUrls: ['./forget-password-drawer.component.less']
-  }
-
-) export class ForgetPasswordDrawerComponent {
-  visible=false;
+@Component({
+  selector: 'app-forget-password-drawer',
+  templateUrl: './forget-password-drawer.component.html',
+  styleUrls: ['./forget-password-drawer.component.less']
+})
+export class ForgetPasswordDrawerComponent {
+  visible = false;
 
   open(): void {
-    this.visible=true;
+    this.visible = true;
   }
 
   close(): void {
-    this.visible=false;
+    this.visible = false;
   }
 }
