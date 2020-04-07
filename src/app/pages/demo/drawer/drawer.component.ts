067d309791f67ccb80eeb26cc7a31280216a69cf
LeonardoRain <295497522@qq.com>
2020-04-07 10:19:05 +0800

    完善 忘记密码&申请注册 提示

--- src/app/pages/demo/drawer/drawer.component.ts
+++ src/app/pages/demo/drawer/drawer.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NzMessageService } from 'ng-zorro-antd';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-drawer',
   templateUrl: './drawer.component.html',
   styleUrls: ['./drawer.component.less']
 })
-export class DrawerComponent {
+export class DrawerComponent implements OnInit {
   visible = false;
+
+  // 注册申请表单 （Registration Request)
+  rrForm = new FormGroup({
+    username: new FormControl(),
+    password: new FormControl(),
+    email: new FormControl(),
+    phone: new FormControl(),
+    nickname: new FormControl(),
+    desc: new FormControl(),
+  });
+
+  constructor(
+    private message: NzMessageService,
+    private fb: FormBuilder,
+  ) { }
+
+  ngOnInit(): void {
+    this.rrForm = this.fb.group({
+      username: [null, [Validators.required]],
+      password: [null, [Validators.required, Validators.minLength(8)]],
+      email: [null, [Validators.required]],
+      phone: [null, [Validators.required]],
+      nickname: [null, [Validators.required]],
+      desc: [null],
+    });
+  }
 
   open(): void {
     this.visible = true;
@@ -14,5 +42,16 @@ export class DrawerComponent {
 
   close(): void {
     this.visible = false;
+  }
+
+  // 请求注册提交提示
+  createRRSMessage(): void {
+    this.message.create('success', `已向管理员发送的您的注册申请！`);
+  }
+
+  // 点击请求注册账号抽屉的提交按钮
+  onClickRRS() {
+    this.createRRSMessage();
+    this.close();
   }
 }
--- src/app/pages/demo/forget-password-drawer/forget-password-drawer.component.ts
+++ src/app/pages/demo/forget-password-drawer/forget-password-drawer.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NzMessageService } from 'ng-zorro-antd';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-forget-password-drawer',
   templateUrl: './forget-password-drawer.component.html',
   styleUrls: ['./forget-password-drawer.component.less']
 })
-export class ForgetPasswordDrawerComponent {
+export class ForgetPasswordDrawerComponent implements OnInit {
   visible = false;
+
+  fpForm = new FormGroup({
+    username: new FormControl(),
+    oldPassword: new FormControl(),
+    newPassword: new FormControl(),
+    confirm: new FormControl(),
+    phoneNumber: new FormControl(),
+    captcha: new FormControl(),
+  });
+
+  constructor(
+    private message: NzMessageService,
+    private fb: FormBuilder
+  ) { }
+
+  ngOnInit(): void {
+    this.fpForm = this.fb.group({
+      username: [null, [Validators.required]],
+      oldPassword: [null, [Validators.required, Validators.minLength(8)]],
+      newPassword: [null, [Validators.required, Validators.minLength(8)]],
+      confirm: [null, [Validators.required, Validators.minLength(8)]],
+      phoneNumber: [null, [Validators.required]],
+      captcha: [null, [Validators.required]],
+    });
+  }
 
   open(): void {
     this.visible = true;
@@ -14,5 +41,16 @@ export class ForgetPasswordDrawerComponent {
 
   close(): void {
     this.visible = false;
+  }
+
+  // 修改密码提交提示
+  createFPSMessage(): void {
+    this.message.create('waring', `该功能暂时不支持，请登陆后修改密码！`);
+  }
+
+  // 点击忘记密码抽屉的提交按钮
+  onclickFPS() {
+    this.createFPSMessage();
+    this.close();
   }
 }
